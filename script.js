fdbbfc27517cbcb8bdcab72ea0e32a9b7b122098
amagavrila-netizen <amagavrila@gmail.com>
2026-07-28 09:37:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 const url = "https://script.google.com/macros/s/AKfycbweEZmfiYZG9bOtKyQKar7bcW-Zv8sC75bvliYN7Eurq94veGujyNfWTxfCDvAfGn7Ndg/exec";
 
-fetch(url)
-.then(res => res.json())
-.then(data => {
+fetch(url )
+  .then(res => res.json())
+  .then(data => {
+    let html = "";
 
-let html = "";
+    // Memulai loop dari i=1 untuk melewati baris judul (header) di Google Sheets
+    for (let i = 1; i < data.length; i++) {
+      html += `
+        <tr>
+          <td>${data[i][0]}</td>   <!-- Rank -->
+          <td>${data[i][1]}</td>   <!-- Nama -->
+          <td>${data[i][2]}</td>   <!-- Main -->
+          <td>${data[i][3]}</td>   <!-- Menang -->
+          <td>${data[i][4]}</td>   <!-- Kalah -->
+          <td>${data[i][5]}</td>   <!-- Frame+ -->
+          <td>${data[i][6]}</td>   <!-- Frame- -->
+          <td>${data[i][7]}</td>   <!-- Selisih -->
+          <td>${data[i][8]}</td>   <!-- Point -->
+          <td>${data[i][9]}</td>   <!-- Avg Margin -->
+          <td>${data[i][10]}</td>  <!-- Penalti -->
+        </tr>
+      `;
+    }
 
-for(let i=1;i<data.length;i++){
-
-html += `
-<tr>
-
-<td>${data[i][0]}</td>
-<td>${data[i][1]}</td>
-<td>${data[i][2]}</td>
-<td>${data[i][3]}</td>
-<td>${data[i][4]}</td>
-<td>${data[i][5]}</td>
-<td>${data[i][6]}</td>
-<td>${data[i][7]}</td>
-<td>${data[i][8]}</td>
-
-</tr>
-`;
-
-}
-
-document.getElementById("isi").innerHTML = html;
-
-});
+    document.getElementById("isi").innerHTML = html;
+  })
+  .catch(err => console.error("Gagal mengambil data:", err));
