48e8702ffd03b5beea4d5d657bb57d3c09c7f41c
amagavrila-netizen <amagavrila@gmail.com>
2026-08-07 08:22:27 +0700

    Update journal-engine.js

--- journal-engine.js
+++ journal-engine.js
@@ -7,38 +7,6 @@ const jurnal = [
 
 {
 title:"📰 LIGA JURNAL",
-  {
-title:"📢 PENGUMUMAN LIGA",
-content:`
-
-<p>
-
-Panitia menginformasikan bahwa <b>9 Pool League Season 1</b> dijadwalkan berakhir pada
-<b>26 Agustus 2026</b>.
-
-</p>
-
-<p>
-
-Seluruh peserta diharapkan menyelesaikan seluruh pertandingan yang masih tersisa sebelum tanggal tersebut agar klasemen akhir dapat ditetapkan secara resmi.
-
-</p>
-
-<p>
-
-Selain itu, seluruh kewajiban administrasi yang berkaitan dengan liga diharapkan segera diselesaikan melalui
-<b>Staf 9 Pool Billiard and Cafe</b>.
-
-</p>
-
-<p>
-
-Terima kasih atas partisipasi dan sportivitas seluruh peserta. Mari selesaikan musim ini dengan semangat fair play hingga pertandingan terakhir.
-
-</p>
-
-`
-},
 content:`
 
 <div class="edition">
